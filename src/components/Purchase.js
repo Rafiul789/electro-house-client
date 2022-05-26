6a35d6bcd2632f85eb41ef177836a8ec300fbd17
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-26 14:23:41 +0600

    order form added to purchase page

--- src/components/Purchase.js
+++ src/components/Purchase.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
+
 import { useParams } from 'react-router-dom';
 import useProductDetails from '../hooks/useProductDetails'
 import {  toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../firebase.init';
+
+
 const Purchase = () => {
     const {id}=useParams();
     const [input, setInput] = useState('')
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const [user, loading, error] = useAuthState(auth)
 
     const [product]=useProductDetails(id)
     
@@ -18,6 +22,10 @@ const Purchase = () => {
         if(input>product.availablequantity){
             toast('Order Availble Quantity')
     }}
+
+    const handleOrder = event =>{
+        event.preventDefault();}
+
 
 
 
@@ -45,16 +53,27 @@ const Purchase = () => {
 </div>
 
     </div> 
-<div className="bg-green-500 mx-5"> 
-<div className="p-5 flex gap-5">
-<label className="label">  <span className="label-text">Product Quantity</span>
-   </label>
-     <input type="number" className="border border-black" value={input} onChange={event => setInput(event.target.value)} />  </div>
+<div className="bg-gray-800 mx-5 p-5"> 
 
-    <div className="items-center mx-5 my-5 ">  <button onClick={() =>inputSubmit()}  className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-   Order
-   <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-</button>  </div>
+     
+     <form onSubmit={handleOrder} className='grid grid-cols-1 gap-3 justify-items-center mt-2'>
+     
+
+                    <input type="number" placeholder="Quantity" className="input input-bordered w-full max-w-xs" value={input} onChange={event => setInput(event.target.value)} />  
+
+                     <input type="text" name="name" disabled value={user?.displayName || ''} className="input input-bordered w-full max-w-xs" /> 
+                        
+                        <input type="email" name="email" disabled value={user?.email || ''} className="input input-bordered w-full max-w-xs" />
+                        <input type="number" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
+                        <input onClick={inputSubmit} type="submit" value="Order" className="btn btn-secondary w-full max-w-xs" />
+                    </form>
+
+
+
+
+
+
+    
       </div>
 
         </div>  </>
